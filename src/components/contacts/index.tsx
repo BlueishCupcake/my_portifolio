514a160584ff11fd47bc8dd3636a1e0dfd20abd5
Sophie <sophiequines@gmail.com>
2021-12-22 11:10:30 -0300

    Removing unused imports

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -2,8 +2,6 @@ import { useTranslation } from "react-i18next";
 
 import Github from "assets/icons/github";
 import Linkedin from "assets/icons/linkedin";
-import Mail from "assets/icons/mail";
-import Phone from "assets/icons/phone";
 
 import { SectionTitle } from "GlobalStyles";
 import * as Styled from "./style";
